62ebebf491811ffc6e63b6245705e0c48a2412b3
Psykopear <psykopear@gmail.com>
2013-11-18 18:20:26 +0100

    updates

--- triangolator.py
+++ triangolator.py
@@ -6,6 +6,33 @@
 
 import os
 import time
+#Random serve solo per provare perche non ho bluetooth
+import random
+from random import randint
+
+matrice = [
+
+[-4 ,-4 ,-4 ,-4 ,-4 ,-4 ,-4 ,-4 ,-4 ,-4 ],
+[-4 ,-3 ,-3 ,-3 ,-3 ,-3 ,-3 ,-3 ,-3 ,-4 ],
+[-4 ,-3 ,-2 ,-2 ,-2 ,-2 ,-2 ,-2 ,-3 ,-4 ],
+[-4 ,-3 ,-2 ,-1 ,-1 ,-1 ,-1 ,-2 ,-3 ,-4 ],
+[-4 ,-3 ,-2 ,-1 , 0 , 0 ,-1 ,-2 ,-3 ,-4 ],
+[-4 ,-3 ,-2 ,-1 , 0 , 0 ,-1 ,-2 ,-3 ,-4 ],
+[-4 ,-3 ,-2 ,-1 ,-1 ,-1 ,-1 ,-2 ,-3 ,-4 ],
+[-4 ,-3 ,-2 ,-2 ,-2 ,-2 ,-2 ,-2 ,-3 ,-4 ],
+[-4 ,-3 ,-3 ,-3 ,-3 ,-3 ,-3 ,-3 ,-3 ,-4 ],
+[-4 ,-4 ,-4 ,-4 ,-4 ,-4 ,-4 ,-4 ,-4 ,-4 ],
+
+]
+
+
+
+
+
+
+punti=[ [0,0,0] ]
+
+modifier = { 0 : (1,0), 1 : (1,1), 2 : (0,1), 3 : (-1,1), 4 : (-1,0), 5 : (-1,-1), 6 : (0,-1), 7 : (1,-1) }
 
 def triangolator() :
  MACS = ["10:BF:48:EA:6A:E2"]
@@ -14,14 +41,17 @@ def triangolator() :
  direzione = 0
  while notarrived:
   for i in MACS:
-   os.system("hcitool cc "+i)
-   time.sleep(1)
-   x = os.system("hcitool rssi "+i+" 2> /dev/null") 
-   if x != 256:
-    x = x.split(': ')[1]
-   else:
-    x = -256
-   os.system("hcitool dc "+i)
+   x = randint(-256,250)
+#vedi anche "rfcomm connect 0 <MAC> <CHANNEL>" per connettersi, forse meglio di hcitool
+#e riconnettere solo in caso non sia piu connesso
+   #os.system("hcitool cc "+i)
+   time.sleep(0.1)
+   #x = os.system("hcitool rssi "+i+" 2> /dev/null") 
+   #if x != 256:
+   # x = x.split(': ')[1]
+   #else:
+   # x = -256
+   #os.system("hcitool dc "+i)
    RSSI.append(x)
   if len(RSSI) > 5:
    RSSI.pop(0)
@@ -33,15 +63,16 @@ def triangolator() :
 
 
 def muoviInDirezione(ar, direzione):
+
 #direzioni:
-# 0 = E
-# 1 = NE
-# 2 = N
-# 3 = NO
-# 4 = O
-# 5 = SO
-# 6 = S
-# 7 = SE
+# 0 = E (+1,0)
+# 1 = NE (+1,+1)
+# 2 = N (0,+1)
+# 3 = NO (-1,+1)
+# 4 = O (-1,0)
+# 5 = SO (-1,-1)
+# 6 = S (0,-1)
+# 7 = SE (+1,-1)
 # 8 = ARRIVATI
  arrived = True
  for i in ar:
@@ -61,17 +92,32 @@ def muoviInDirezione(ar, direzione):
    else: 
     all1 = all1 + 1
   j = j+1
+ xcoord = punti[-1][0] + modifier[direzione][0]
+ ycoord = punti[-1][1] + modifier[direzione][1]
+# for i in punti:
+#   if i[0] == xcoord and i[1] == ycoord:
+#    return direzione
  if avv1 > all1:
   print dev
   print "Ti stai avvicinando al device"
+  punti.append([xcoord,ycoord,punti[-1][2]+1])
+  print punti
   print "Muovi in direzione "+`direzione`
   return direzione
  else:
-  print dev
+  direzione = (direzione + 2) % 8
+  xcoord = punti[-1][0] + modifier[direzione][0]
+  ycoord = punti[-1][1] + modifier[direzione][1]
+  for i in punti:
+   if i[0] == xcoord and ycoord == i[1] and i[2] < punti[-1][2]:
+    direzione = (direzione + 1) % 8
+    xcoord = punti[-1][0] + modifier[direzione][0]
+    ycoord = punti[-1][1] + modifier[direzione][1]
+  punti.append([xcoord,ycoord,punti[-1][2]-1])
+  print punti
   print "Ti stai allontanando dal device, cambio direzione..."
-  print "Muovi in direzione "+`(direzione+1)%8`
-  return (direzione + 1)%8
- 
+  print "Muovi in direzione "+`direzione`
+  return direzione
 
 triangolator()
    
